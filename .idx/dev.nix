ac2fc25b6949c1f5dd2ab3fc645ecb38467524e0
vinukondadheeraj <vinukondadheeraj@gmail.com>
2025-07-17 02:54:24 +0000

    Configure environment for Android development

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,57 +1,38 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{
+{ pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # or "stable-24.05"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    "android-sdk"
-    # "go"
-    # "python311"
-    # "python311Packages.pip"
-    # "nodejs_20"
-    # "nodePackages.nodemon"
+    pkgs.nodejs_20
+    pkgs.androidsdk
   ];
+
+  # Allow unfree packages and accept the Android SDK license
+  nixpkgs.config = {
+    allowUnfree = true;
+    android_sdk.accept_license = true;
+  };
 
   # Sets environment variables in the workspace
   env = {
-    ANDROID_HOME = "$ANDROID_SDK_ROOT";
+    ANDROID_HOME = "${pkgs.androidsdk}/share/android-sdk";
   };
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
 
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
     # Workspace lifecycle hooks
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
         # Example: install JS dependencies from NPM
-        # "npm-install" = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # "watch-backend" = "npm run watch-backend";
+        npm-install = "cd ToufAI && npm install";
       };
     };
   };
